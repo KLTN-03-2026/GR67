7a6c86c2d7bf9ceae6d38b4c47e634f043c24220
kazuto <congtrinhotaku2@gmail.com>
2026-03-29 20:39:37 +0700

    fix lỗi đăng nhập

--- server/models/NguoiDung.js
+++ server/models/NguoiDung.js
@@ -54,11 +54,10 @@ const nguoiDungSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // HASH PASSWORD
-nguoiDungSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+nguoiDungSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // compare password
